e34cffd1622c75db4fbcf471e013f8a5f1f7bb8d
Charlie Moran <Carlosamoran18@gmail.com>
2022-09-06 13:39:15 -0400

    index change

--- portfolio-project/src/Pages/Game.js
+++ portfolio-project/src/Pages/Game.js
@@ -4,12 +4,14 @@ function Game() {
 
   return (<section>
   <h1 className="game">Game go brrrrrr</h1>
+  <div>
   <meta charset="utf-8" />
     <title> Hop Off </title>
     <script type="text/javascript" src="phaser.min.js"></script>
     <script type="text/javascript" src="game.js"></script>
 
     <meta name="viewport" content="initial-scale=1 user-scalable=0" />
+  </div>
   </section>
   );
 }
