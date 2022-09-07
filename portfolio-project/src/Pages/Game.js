bf35c85591fc056b65f074c8c2870cd0c8e5249b
Charlie Moran <Carlosamoran18@gmail.com>
2022-09-07 01:50:50 -0400

    class fix

--- portfolio-project/src/Pages/Game.js
+++ portfolio-project/src/Pages/Game.js
@@ -9,7 +9,7 @@ function Game() {
     <title> Hop Off </title>
     <script type="text/javascript" src="phaser.min.js"></script>
     <script type="text/javascript" src="game.js"></script>
-    <div class="vsc-initialized" cz-shortcut-listen="true">
+    <div className="vsc-initialized" cz-shortcut-listen="true">
     <script defer="" src="game.js" type="text/javascript"></script>
     <canvas width="384" height="224" id="game-blocky"></canvas></div>
     <meta name="viewport" content="initial-scale=1 user-scalable=0" />
